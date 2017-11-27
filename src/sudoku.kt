e3549d2bdbfb1feebf585de7ad983fc82f549cae
Able Shi <zaixing.a.shi@vanderbilt.edu>
2017-11-27 15:21:35 -0600

    reading file with BufferReader

--- src/sudoku.kt
+++ src/sudoku.kt
@@ -1,9 +1,21 @@
 import java.io.File
 import java.io.InputStream
 
-fun main(args: Array<String>) {
-    val inputStream: InputStream = File("sudoku-test1.txt").inputStream()
+//int board[9][9];
 
-    val inputString = inputStream.bufferedReader().use { it.readText() }
-    println(inputString)
+fun main(args: Array<String>) {
+//    val inputStream: InputStream = File("sudoku-test1.txt").inputStream()
+//
+//    val inputString = inputStream.bufferedReader().use { it.readText() }
+//    println(inputString)
+    loadFromFile()
+}
+
+fun loadFromFile() {
+    val file = File("sudoku-test1.txt")
+    val bufferedReader = file.bufferedReader()
+    val text:List<String> = bufferedReader.readLines()
+    for(line in text){
+        println(line)
+    }
 }
